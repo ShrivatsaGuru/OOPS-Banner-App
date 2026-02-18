81310631f5eaa56c511115efb1ad873c9e50dadd
Shrivatsa Guru <shrivatsaindra77@gmail.com>
2026-02-18 16:12:19 +0530

    Printed OOPS using String Arrays and Flower brackets

--- OOPS.java
+++ OOPS.java
@@ -1,29 +1,27 @@
 /**
- *This class demonstrates a simple java application that displayed OOPS using an array of strings.
+ *This class demonstrates a simple java application that displayed OOPS using an array of strings and flower brackets.
  *@author Developer
- *@version 4
+ *@version 5
  */
 class OOPSstar
 {
 	public static void main(String[] args)
 	{
-		String [] lines=new String[11];
-		
-		lines[0]=String.join("", "    ***    " , " " , "    ***    "  , " " , "*********  " , " " , "       *******");
-		lines[1]=String.join("", "  *** ***  " , " " , "  *** ***  "  , " " , "*        * " , " " , "    **   ");
-		lines[2]=String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*         *" , " " , "  ***     ");
-		lines[3]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*         *" , " " , " **          ");
-		lines[4]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*        * " , " " , " *          ");
-		lines[5]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*       *  " , " ", "   *****");
-		lines[6]=String.join("", "***     ***" , " " , "***     ***"  , " " , "********   " , " " , "       **");
-		lines[7]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*          " , " " , "         ***");
-		lines[8]=String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*          " , " " , "            ***" );
-		lines[9]=String.join("", "  *** ***  " , " " , "  *** ***  "  , " " , "*          " , " " , "           **" );
-		lines[10]=String.join("","    ***    " , " " , "    ***    "  , " " , "*          " , " " , "***********" );
+		String [] lines={"    ***    " , " " , "    ***    "  , " " , "*********  " , " " , "       *******","\n"
+        ,"  *** ***  " , " " , "  *** ***  "  , " " , "*        * " , " " , "    **   ","\n",
+        " ***   *** " , " " , " ***   *** "  , " " , "*         *" , " " , "  ***     ","\n",
+        "***     ***" , " " , "***     ***"  , " " , "*         *" , " " , " **          ","\n",
+        "***     ***" , " " , "***     ***"  , " " , "*        * " , " " , " *          ","\n",
+        "***     ***" , " " , "***     ***"  , " " , "*       *  " , " ", "   *****","\n",
+        "***     ***" , " " , "***     ***"  , " " , "********   " , " " , "       **","\n",
+        "***     ***" , " " , "***     ***"  , " " , "*          " , " " , "         ***","\n",
+        " ***   *** " , " " , " ***   *** "  , " " , "*          " , " " , "            ***","\n",
+        "  *** ***  " , " " , "  *** ***  "  , " " , "*          " , " " , "           **","\n",
+        "    ***    " , " " , "    ***    "  , " " , "*          " , " " , "***********"};
 
 		for(String s:lines)
 		{
-			System.out.println(s);
+			System.out.print(s);
 		}
 	}
 }
