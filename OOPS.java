c8b9d339835d26eedd3a01824203a674ab5974d2
Shrivatsa Guru <shrivatsaindra77@gmail.com>
2026-02-18 15:50:30 +0530

    Used String Arrays to print OOPS

--- OOPS.java
+++ OOPS.java
@@ -1,26 +1,29 @@
 /**
- *This class demonstrates a simple java application that displayed OOPS and also printed it using 
- *String.join() method, making use of delimiter to print a sentence with spaces and OOPS.
+ *This class demonstrates a simple java application that displayed OOPS using an array of strings.
  *@author Developer
- *@version 3
+ *@version 4
  */
-
-
-class Message
+class OOPSstar
 {
 	public static void main(String[] args)
 	{
-		System.out.println(String.join("", "    ***    " , " " , "    ***    "  , " " , "*********  " , " " , "***********"));
-		System.out.println(String.join("","   *** ***  " , " " , "  *** ***  "  , " " , "*        * " , " " , "*          "));
-		System.out.println(String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*         *" , " " , "*          "));
-		System.out.println(String.join("", "***     ***" , " " , "***     ***"  , " " , "*         *" , " " , "*          "));
-		System.out.println(String.join("", "***     ***" , " " , "***     ***"  , " " , "*        * " , " " , "*          "));
-		System.out.println(String.join("", "***     ***" , " " , "***     ***"  , " " , "*       *  " , " ", "***********"));
-		System.out.println(String.join("", "***     ***" , " " , "***     ***"  , " " , "********   " , " " , "          *"));
-		System.out.println(String.join("", "***     ***" , " " , "***     ***"  , " " , "*          " , " " , "          *"));
-		System.out.println(String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*          " , " " , "          *" ));
-		System.out.println(String.join("", "  *** ***  " , " " , "  *** ***  "  , " " , "*          " , " " , "          *" ));
-		System.out.println(String.join("", "    ***    " , " " , "    ***    "  , " " , "*          " , " " , "***********" ));
-		System.out.println(String.join(" ", "Hi","My","Name","Is","Shrivatsa"));
+		String [] lines=new String[11];
+		
+		lines[0]=String.join("", "    ***    " , " " , "    ***    "  , " " , "*********  " , " " , "       *******");
+		lines[1]=String.join("", "  *** ***  " , " " , "  *** ***  "  , " " , "*        * " , " " , "    **   ");
+		lines[2]=String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*         *" , " " , "  ***     ");
+		lines[3]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*         *" , " " , " **          ");
+		lines[4]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*        * " , " " , " *          ");
+		lines[5]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*       *  " , " ", "   *****");
+		lines[6]=String.join("", "***     ***" , " " , "***     ***"  , " " , "********   " , " " , "       **");
+		lines[7]=String.join("", "***     ***" , " " , "***     ***"  , " " , "*          " , " " , "         ***");
+		lines[8]=String.join("", " ***   *** " , " " , " ***   *** "  , " " , "*          " , " " , "            ***" );
+		lines[9]=String.join("", "  *** ***  " , " " , "  *** ***  "  , " " , "*          " , " " , "           **" );
+		lines[10]=String.join("","    ***    " , " " , "    ***    "  , " " , "*          " , " " , "***********" );
+
+		for(String s:lines)
+		{
+			System.out.println(s);
 		}
+	}
 }
