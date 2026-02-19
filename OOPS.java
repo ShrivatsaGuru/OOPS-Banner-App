8d22f5af4e2e8b8e10d0343ab7735148e58719f8
Shrivatsa Guru <shrivatsaindra77@gmail.com>
2026-02-19 09:57:59 +0530

    Printed OOPS using static methods

--- OOPS.java
+++ OOPS.java
@@ -1,27 +1,53 @@
 /**
- *This class demonstrates a simple java application that displayed OOPS using an array of strings and flower brackets.
- *@author Developer
- *@version 5
- */
-class OOPSstar
-{
-	public static void main(String[] args)
-	{
-		String [] lines={"    ***    " , " " , "    ***    "  , " " , "*********  " , " " , "       *******","\n"
-        ,"  *** ***  " , " " , "  *** ***  "  , " " , "*        * " , " " , "    **   ","\n",
-        " ***   *** " , " " , " ***   *** "  , " " , "*         *" , " " , "  ***     ","\n",
-        "***     ***" , " " , "***     ***"  , " " , "*         *" , " " , " **          ","\n",
-        "***     ***" , " " , "***     ***"  , " " , "*        * " , " " , " *          ","\n",
-        "***     ***" , " " , "***     ***"  , " " , "*       *  " , " ", "   *****","\n",
-        "***     ***" , " " , "***     ***"  , " " , "********   " , " " , "       **","\n",
-        "***     ***" , " " , "***     ***"  , " " , "*          " , " " , "         ***","\n",
-        " ***   *** " , " " , " ***   *** "  , " " , "*          " , " " , "            ***","\n",
-        "  *** ***  " , " " , "  *** ***  "  , " " , "*          " , " " , "           **","\n",
-        "    ***    " , " " , "    ***    "  , " " , "*          " , " " , "***********"};
-
-		for(String s:lines)
-		{
-			System.out.print(s);
+* This class is a simple java function that prints oops using a differet static method fuction with each letter being printed seperately
+* @author Developer
+* @version 6.0
+**/
+class OOPS{
+	public static void main(String[] args){
+		String letterO[]=PrintLetter.printOLetter();
+		String letterP[]=PrintLetter.printPLetter();
+		String letterS[]=PrintLetter.printSLetter();
+		for(int i=0;i<9;i++){
+			System.out.println(letterO[i]+"  "+letterO[i]+"  "+letterP[i]+"  "+letterS[i]);
 		}
+	}
+}
+class PrintLetter{
+	public static String[] printOLetter(){
+		String lines[]={"     ***   ",
+						"   **   ** ",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"   **   ** ",
+						"     ***   "};
+		return lines;
+	}
+	public static String[] printPLetter(){
+		String lines[]={"*******",
+						"**    *",
+						"**    *",
+						"**    *",
+						"*******",
+						"**     ",
+						"**     ",
+						"**     ",
+						"**     "};
+		return lines;
+	}
+	public static String[] printSLetter(){
+		String lines[]={"   ***** ",
+						" **      ",
+						"**       ",
+						" **      ",
+						"   ***   ",
+						"      ** ",
+						"       **",
+						"      ** ",
+						" *****   "};
+		return lines;
 	}
 }
